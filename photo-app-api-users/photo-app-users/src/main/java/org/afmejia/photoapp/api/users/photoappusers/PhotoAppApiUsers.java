43f7963c61cc5eb7d3897df44acedd2376b31061
Felipe <andres@rise.agency>
2019-07-02 12:25:37 -0500

    users microservice added

---
+++ photo-app-api-users/photo-app-users/src/main/java/org/afmejia/photoapp/api/users/photoappusers/PhotoAppApiUsers.java
@@ -0,0 +1,13 @@
+package org.afmejia.photoapp.api.users.photoappusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiUsers {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUsers.class, args);
+	}
+
+}
