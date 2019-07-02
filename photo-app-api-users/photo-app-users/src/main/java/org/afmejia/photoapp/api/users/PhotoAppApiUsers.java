6e944f1d6bfda289cc2743fc3ae2ea1ab7cc8d1f
Felipe <andres@rise.agency>
2019-07-02 12:50:33 -0500

    Creating basic user controller

--- photo-app-api-users/photo-app-users/src/main/java/org/afmejia/photoapp/api/users/PhotoAppApiUsers.java
+++ photo-app-api-users/photo-app-users/src/main/java/org/afmejia/photoapp/api/users/PhotoAppApiUsers.java
@@ -1,9 +1,11 @@
-package org.afmejia.photoapp.api.users.photoappusers;
+package org.afmejia.photoapp.api.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppApiUsers {
 
 	public static void main(String[] args) {
