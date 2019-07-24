8e2f5f584139ffb49f61e137427d11f0378342cd
Felipe <afmejia23@utp.edu.co>
2019-07-24 05:06:50 -0500

    XML Support added

--- photoapp-api-users-application/src/main/java/org/afmejia/photoapp/api/users/security/WebSecurity.java
+++ photoapp-api-users-application/src/main/java/org/afmejia/photoapp/api/users/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package org.afmejia.photoapp.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	
 	Environment environment;
 	
+	@Autowired
 	public WebSecurity(Environment environment) {
 		this.environment = environment;
 	}
@@ -19,7 +21,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure (HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
 		http.headers().frameOptions().disable();
 	}
 }
--- photoapp-api-users-application/src/main/java/org/afmejia/photoapp/api/users/ui/controllers/UsersController.java
+++ photoapp-api-users-application/src/main/java/org/afmejia/photoapp/api/users/ui/controllers/UsersController.java
@@ -11,6 +11,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +34,10 @@ public class UsersController {
         return "Working on port " + env.getProperty("local.server.port");
     }
     
-    @PostMapping
+    @PostMapping(
+    		consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+    		produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+    )
     public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
     	ModelMapper modelMapper = new ModelMapper();
     	modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
